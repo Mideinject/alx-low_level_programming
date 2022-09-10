d760cdb995bd8da653e95004e63a6a54176ccd9e
Mideinject <olamideaaron0@gmail.com>
2022-09-10 14:42:41 -0700

    omo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+1 #include <studio.h>
+2 
+3 /**
+    * main - Entry point
+    * 
+    * input: prints "Programming is like building a multilingual puzzle,"
+    *
+    * Return: Alaways 0 (Success)
+    */
+4 int main(void)
+5 {
+	puts("\n"Programming is like building a multilingual puzzle,");
+6 return (0);
