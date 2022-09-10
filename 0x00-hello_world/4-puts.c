cd6093a3efc573ec9b6b506499b816df8fad58ba
Mideinject <olamideaaron0@gmail.com>
2022-09-10 15:45:53 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
-1#!/bin/bash
-
-3 #include <studio.h>
-4 
-5 /**
-    * main - Entry point
-    * 
-    * input: prints "Programming is like building a multilingual puzzle,"
-    *
-    * Return: Alaways 0 (Success)
-    */
-6 int main(void)
-7 {
+1| #!/bin/bash
+2| 
+3| #include <studio.h>
+4|
+5| /**
+6|   * main - Entry point
+7|   * 
+8|   * input: prints "Programming is like building a multilingual puzzle,"
+9|   *
+10|  * Return: Alaways 0 (Success)
+11|  */
+12|   int main(void)
+13| {
 	puts("\n"Programming is like building a multilingual puzzle,");
-	return (0);
-8 }
+15|	return (0);
+16| }
