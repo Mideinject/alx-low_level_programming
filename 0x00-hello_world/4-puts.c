eaf03e857c08ce62bb9e5c9bc9f3fa15e897f451
Mideinject <olamideaaron0@gmail.com>
2022-09-11 02:41:42 -0700

    nawah

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,11 @@
  *
  *  * main - Entry point
  *
- *   *
+ *  
  *
  *    * Description: prints "Programming is like building a multilingual puzzle"
  *
- *     *
+ *    
  *
  *      * Return: Always 0 (Success)
  *
