06cd69b8d1bd0b483f39de6d2dcf9b29dc925d54
Mideinject <olamideaaron0@gmail.com>
2022-09-11 04:41:04 -0700

    yeah

---
+++ 0x00-hello_world/101-quite.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print quote
+ *
+ * Detail: prints Dor's quote
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	write(2, "and tht piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59);
+	return (1)
+}
